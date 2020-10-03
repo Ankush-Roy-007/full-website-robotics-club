57c4e9f88d4f81809e225eafb431974f4a2e397f
Ankush-Roy-007 <ankushroy44@yahoo.com>
2020-10-03 09:22:26 +0530

    final

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session')
 const passport=require("passport");
 const passportLocalMongoose=require("passport-local-mongoose");
 const _ = require("lodash");
+const multer=require("multer");
+const path=require("path");
+const fs=require("fs");
 
 
 const app=express();
@@ -22,7 +25,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb+srv://AnKuSh-07:qwerasd1234@robotics-club.ux1dt.mongodb.net/Club?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://aitcear:aitrobotics@1234@robotics-club-website-d.wkwhk.mongodb.net/Club?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true });
 
 
 const blogSchema=new mongoose.Schema({
@@ -280,6 +283,191 @@ app.get("/delete/:memberid",function(req,res){
     }
   });
 });
+
+
+app.get("/managertestemonialcreate",function(req,res){
+  res.render("managertestemonialcreate");
+});
+
+const testemonialuploadSchema=new mongoose.Schema({
+  Editor_name:String,
+  Member_firstname:String,
+  Member_Regn_No:String,
+  Member_Year:String,
+  Member_Branch:String,
+  Member_Domain:String,
+  Member_Phone_no:String,
+  Member_Email:String,
+  Image_name:String,
+  contentType:String,
+  path:String,
+  Imagetestemonial:Buffer,
+});
+
+const testemonialupload=new mongoose.model("testemonials",testemonialuploadSchema);
+const storagetestemonial=multer.diskStorage({
+  destination:function(req,file,cb){
+    cb(null,'public/uploadstestemonial');
+  },
+  filename:function(req,file,cb){
+    cb(null,file.fieldname + '-'+Date.now()+path.extname(file.originalname));
+  }
+})
+    const uploadimg=multer({
+      storage:storagetestemonial
+    });
+app.post("/uploadtestemonial",uploadimg.single("testemonialimg"),function(req,res){
+  testemonialupload.findOne({Member_Regn_No:req.body.testemonialregn},function(err,deletefound){
+    console.log(deletefound);
+    if(deletefound!=null){
+      console.log(req.file.path);
+      fs.unlinkSync("public/uploadstestemonial/"+deletefound.Image_name);
+      console.log("deleted photo");
+      testemonialupload.deleteOne({Member_Regn_No:req.body.testemonialregn},function(err){
+        if(err){
+          console.log("image delete error");
+        }
+      });
+    }
+  });
+  registerentry.findOne({ username:req.body.testemonialregn},function(err,found){
+
+    if(err){
+      console.log("error")
+    }
+    else{
+      const img=fs.readFileSync(req.file.path);
+const encode_image=img.toString("base64");
+    const done=new testemonialupload({
+  Editor_name:req.body.username,
+  Member_firstname:found.First_Name+" "+found.Last_name,
+  Member_Regn_No:found.username,
+  Member_Year:found.Year,
+  Member_Branch:found.Branch,
+  Member_Domain:found.Domain,
+  Member_Phone_no:found.Phone_No,
+  Member_Email:found.E_mail,
+  Image_name:req.file.filename,
+    })
+    done.save();
+    res.send("successfully saved")
+
+  }
+});
+});
+
+app.get("/show",function(req,res){
+  testemonialupload.find({},function(err,found){
+    if(err){
+      console.log("bad")
+    }
+    else{
+      res.render("gallary",{
+        found:found 
+      });
+      
+    }
+    
+  });
+});
+
+
+app.get("/updatemember",function(req,res){
+  if(req.isAuthenticated()&&req.user.Authorization=="Memmber"){
+  res.render("updatemember",{
+    user_name:req.user.First_Name,
+      idfound:req.user
+  })
+}
+});
+
+///////////////////////////////////////////////////
+app.get("/albumcreate",function(req,res){
+  res.render("albumcreate");
+});
+
+const AlbumphotoSchema=new mongoose.Schema({
+  Editor_name:String,
+  Editor_Regn_no:String,
+  Date:String,  
+  Image_name:String,
+  Approval:String,
+});
+
+const Albumphotoupload=new mongoose.model("albumphoto",AlbumphotoSchema);
+const storagealbum=multer.diskStorage({
+  destination:function(req,file,cb){
+    cb(null,'public/uploadsalbum');
+  },
+  filename:function(req,file,cb){
+    cb(null,file.fieldname + '-'+Date.now()+path.extname(file.originalname));
+  }
+})
+    const uploadalbumimg=multer({
+      storage:storagealbum
+    });
+app.post("/uploadalbumimg",uploadalbumimg.single("albumimg"),function(req,res){
+  const done=new Albumphotoupload({
+  Editor_name:req.user.First_Name+" "+req.user.Last_name,
+  Editor_Regn_no:req.user.username,
+  Date:req.body.uploadimgalbumdate,  
+  Image_name:req.file.filename,
+  Approval:"Not-Approved",
+});
+  done.save();
+  res.redirect("register",{
+    messagereg:"Request Admin for Approval !"
+  });
+});
+
+app.get("/show",function(req,res){
+  testemonialupload.find({},function(err,found){
+    if(err){
+      console.log("bad")
+    }
+    else{
+      res.render("gallary",{
+        found:found 
+      });
+      
+    }
+    
+  });
+});
+
+
+app.get("/df",function(req,res){
+  
+  res.render("imageapproval");
+
+});
+const send=require(__dirname+"/mail");
+
+app.get("/snd",function(req,res){ 
+var x=send("royankush399@gmail.com","You have sucessfully implemented this..!","Ankush");
+console.log(x);
+})
+
+
+app.get("/s",function(req,res){
+  res.render("mailsend",{
+    messagereg:"success"
+  })
+})
+
+
+  app.get("/b",function(req,res){
+    blogrentry.find({Approval:"Approved"},function(err,found){
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.render("approvedblogs",{
+          found:found
+        });
+      }
+    });
+  })
 
 
 app.listen(3000,function(){
